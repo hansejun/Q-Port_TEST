345d35d00da6a72567a1e077465a5acd7b4bcb2d
hansejun <tpwnszja2@gmail.com>
2022-10-23 04:16:55 +0900

    망

---
+++ src/pages/QuestionDetail.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled/types/base";
+import Layout from "../components/Layout/Layout";
+
+function QuestionDetail() {
+  return (
+    <Layout>
+      <Wrapper as="main"></Wrapper>
+    </Layout>
+  );
+}
+export default QuestionDetail;
+
+const Wrapper = styled.div``;
