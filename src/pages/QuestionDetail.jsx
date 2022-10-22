22256ef7f85ede2782d3033a841dbcdc3246f7a6
hansejun <tpwnszja2@gmail.com>
2022-10-23 04:38:08 +0900

    질문 디테일 페이지 생성

---
+++ src/pages/QuestionDetail.jsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout/Layout";
+
+function QuestionDetail() {
+  return (
+    <Layout>
+      <div>Question</div>
+    </Layout>
+  );
+}
+export default QuestionDetail;
